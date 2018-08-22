24261d2853709231514d97f63ee3263f2570e9c1
bakamoridesu <bakamoridesu@gmail.com>
2018-08-22 20:48:34 +0300

    Completed the Predict, Update and UpdateEKF functions. Implemented RMSE calculation. RMSE values ~ [0.1, 0.09, 0.45, 0.43]

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -25,6 +25,9 @@ void KalmanFilter::Predict() {
   TODO:
     * predict the state
   */
+  x_ = F_ * x_;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
@@ -32,6 +35,19 @@ void KalmanFilter::Update(const VectorXd &z) {
   TODO:
     * update the state by using Kalman Filter equations
   */
+  VectorXd z_pred = H_ * x_;
+  VectorXd y = z - z_pred;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
@@ -39,4 +55,32 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   TODO:
     * update the state by using Extended Kalman Filter equations
   */
+  // get polar coordinates
+  double ro = sqrt(x_(0)*x_(0) + x_(1)*x_(1));
+  double theta = atan2(x_(1), x_(0));
+  double ro_dot = 0.0;
+  // avoid division by zero
+  if(ro > 0.0001){
+    ro_dot = (x_(0) * x_(2) + x_(1) * x_(3))/ro;
+  }
+  // initialize h(x)
+  VectorXd h_x = VectorXd(3);
+  h_x << ro, theta, ro_dot;
+  // measurement update
+  VectorXd y = z - h_x;
+  // adjusting  -pi < phi < pi
+  if(abs(y(1)) > M_PI ){
+    y(1) -= round(y(1) / (2.0 * M_PI)) * (2.0 * M_PI);
+  }
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
